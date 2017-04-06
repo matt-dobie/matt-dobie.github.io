4fb8946525d1913ff618f643ceba0952b4beaeaa
Matt Dobie <mattdobie23@gmail.com>
2017-04-06 12:32:38 +1000

    Add memegenerator to projectBuilder

--- js/projectBuilder.js
+++ js/projectBuilder.js
@@ -2,16 +2,10 @@
 var projects = {
     "projects": [
         {
-            "title": "Portfolio Website",
-            "description": "This very page, mattdobie.com, is my first attempt at building a functional website. Its purpose is to to display my completed and future development projects",
-            "url": "index.html",
-            "image": "images/portfoliowebsite.jpg"
-        },
-        {
-            "title": "Falcon Assault",
-            "description": "A simple game developed in Scratch, where the goal is to pilot the Millennium Falcon and survive as long as you can against incoming waves of enemy TIE fighters!",
-            "url": "https://scratch.mit.edu/projects/106309939/",
-            "image": "images/falconassualt.jpg"
+            "title": "Meme Generator",
+            "description": "This project was also developed with help from Udacity, and allows the user to generate their own hilarious memes using any photo they choose",
+            "url": "portfolio/memegenerator.html",
+            "image": "images/memegenerator.jpg"
         },
         {
             "title": "Traffic Bugs",
@@ -20,10 +14,16 @@ var projects = {
             "image": "images/trafficbugs.jpg"
         },
         {
-            "title": "Coming Soon",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper, velit eget porta malesuada, justo magna condimentum nibh, at dapibus massa enim varius nisl",
-            "url": "#",
-            "image": "http://placehold.it/400x250"
+            "title": "Falcon Assault",
+            "description": "A simple game developed in Scratch, where the goal is to pilot the Millennium Falcon and survive as long as you can against incoming waves of enemy TIE fighters!",
+            "url": "https://scratch.mit.edu/projects/106309939/",
+            "image": "images/falconassualt.jpg"
+        },
+        {
+            "title": "Portfolio Website",
+            "description": "This very page, mattdobie.com, is my first attempt at building a functional website. Its purpose is to to display my completed and future development projects",
+            "url": "index.html",
+            "image": "images/portfoliowebsite.jpg"
         },
         {
             "title": "Coming Soon",
