82709b8dbb4bd586e3250a6ced9226b53e1f578b
Matt Dobie <mattdobie23@gmail.com>
2017-04-11 14:01:49 +1000

    Add Pitch Perfect app to projects

--- js/projectBuilder.js
+++ js/projectBuilder.js
@@ -2,10 +2,10 @@
 var projects = {
     "projects": [
         {
-            "title": "Meme Generator",
-            "description": "This project was also developed with help from Udacity, and allows the user to generate their own hilarious memes using any photo they choose",
-            "url": "portfolio/memegenerator.html",
-            "image": "images/memegenerator.jpg"
+            "title": "Pitch Perfect",
+            "description": "My very first iOS application. Developed with help from Udacity, the application allows the user to add simple audio effects to a recording of their own voice",
+            "url": "https://github.com/matt-dobie/udacity-pitch-perfect",
+            "image": "images/pitchperfect.jpg"
         },
         {
             "title": "Traffic Bugs",
@@ -14,22 +14,22 @@ var projects = {
             "image": "images/trafficbugs.jpg"
         },
         {
-            "title": "Falcon Assault",
-            "description": "A simple game developed in Scratch, where the goal is to pilot the Millennium Falcon and survive as long as you can against incoming waves of enemy TIE fighters!",
-            "url": "https://scratch.mit.edu/projects/106309939/",
-            "image": "images/falconassualt.jpg"
-        },
-        {
             "title": "Portfolio Website",
             "description": "This very page, mattdobie.com, is my first attempt at building a functional website. Its purpose is to to display my completed and future development projects",
             "url": "index.html",
             "image": "images/portfoliowebsite.jpg"
         },
         {
-            "title": "Coming Soon",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper, velit eget porta malesuada, justo magna condimentum nibh, at dapibus massa enim varius nisl",
-            "url": "#",
-            "image": "http://placehold.it/400x250"
+            "title": "Meme Generator",
+            "description": "This project was also developed with help from Udacity, and allows the user to generate their own hilarious memes using any photo they choose",
+            "url": "portfolio/memegenerator.html",
+            "image": "images/memegenerator.jpg"
+        },
+        {
+            "title": "Falcon Assault",
+            "description": "A simple game developed in Scratch, where the goal is to pilot the Millennium Falcon and survive as long as you can against incoming waves of enemy TIE fighters!",
+            "url": "https://scratch.mit.edu/projects/106309939/",
+            "image": "images/falconassualt.jpg"
         },
         {
             "title": "Coming Soon",
